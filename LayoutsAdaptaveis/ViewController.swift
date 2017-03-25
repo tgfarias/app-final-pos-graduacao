30049ee2ff0d60c2cac7c4c34fba65db6b850615
Paulo Canedo <paulocanedo@gmail.com>
2017-03-25 08:36:32 -0300

    Initial Commit

---
+++ LayoutsAdaptaveis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutsAdaptaveis
+//
+//  Created by Faculdade Catolica do Tocantins on 25/03/17.
+//  Copyright © 2017 FCTecnologia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
